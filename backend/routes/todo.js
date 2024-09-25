1bd24f6ce84bde6f7c63f023f5334e8a421a30d0
Phanindra babu <109127892+Phanindrababu5868@users.noreply.github.com>
2024-09-25 11:38:51 +0530

    Update todo.js

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const TODO = require("../models/TODO");
+const TODO = require("../models/Todo");
 const auth = require("../middleware/auth");
 
 const router = express.Router();
